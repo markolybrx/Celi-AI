fdd2ff75b4f517d24a33a02a0f73662c526c58a8
markolybrx <132954069+markolybrx@users.noreply.github.com>
2026-02-02 02:37:54 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start Celery
-celery -A app.celery_app worker --loglevel=info &
-
-# Start Gunicorn in Standard Mode (No Gevent)
-exec gunicorn app:app --bind 0.0.0.0:$PORT --timeout 120
