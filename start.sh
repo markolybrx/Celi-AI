f26a500eb08ae8f2d2919a4dfafcda0a84bb8a57
markolybrx <132954069+markolybrx@users.noreply.github.com>
2026-02-07 07:56:32 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Start Celery
-celery -A app.celery_app worker --loglevel=info &
-
-# Start Gunicorn in Standard Mode (No Gevent)
+# Start Gunicorn in Standard Mode
+# We have removed the Celery worker to support Serverless/Render deployments
 exec gunicorn app:app --bind 0.0.0.0:$PORT --timeout 120
